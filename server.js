9f21acf9461298eb2d9b1fc8869d6bd192b4bea7
jaolsen7 <jaolsen@berkeley.edu>
2022-01-30 21:18:36 -0800

    got rid of fsUtils and wrote fs.writeFile

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils.js');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+var db = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,41 +11,48 @@ const app = express();
 // Code for Middleware:
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Notes route
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // GET Notes res.json from db
-app.get('/api/notes', (req, res) => {
-    res.json('/db/db.json')
+app.get("/api/notes", (req, res) => {
+  res.json("/db/db.json");
 });
 
 // POST Notes
-app.post('/api/notes', (req, res) => {
-    const { id, title, text } = req.body;
+app.post("/api/notes", (req, res) => {
+  const { id, title, text } = req.body;
 
-    if (id && title && text) {
-        const newNote = {
-            id,
-            title,
-            text,
-          };
-          // Function to read file, parse json, push to array, stringify array, write file
-          readAndAppend(newNote, '/db/db.json');
-          res.json(`Note added successfully 🚀`);
-        } else {
-          res.error('Error in adding tip');
-        }
+  if (title && text) {
+    const newNote = {
+      id: uuid.v1(),
+      title,
+      text,
+    };
+
+    db.push(newNote);
+
+    fs.writeFile("./db/db.json", JSON.stringify(db), (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `${newNote.title} has been written to JSON file`
+          )
+    );
+
+    res.status(201).json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding new note...");
+  }
 });
 
 // else GET Index, should go last
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at PORT: ${PORT} 🚀`)
-);
+app.listen(PORT, () => console.log(`App listening at PORT: ${PORT} 🚀`));
